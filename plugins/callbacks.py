8d8b20851d8d8fe44636fc3529ee9186367746d6
Manjunath Bhat <86521081+Bhatmanjusms@users.noreply.github.com>
2022-10-28 22:33:27 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -30,7 +30,7 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("𝘓𝘦𝘵’𝘴 𝘳𝘰𝘭𝘭", url=f'http://t.me/?startgroup=true')
+                    InlineKeyboardButton("𝘓𝘦𝘵’𝘴 𝘳𝘰𝘭𝘭", url=f'http://t.me/Thunder_filterbot?startgroup=true')
                     ],[
                     InlineKeyboardButton("❓ 𝘏𝘦𝘭𝘱 ❓", callback_data="help_data")
                 ]
